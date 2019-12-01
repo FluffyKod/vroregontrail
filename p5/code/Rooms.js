44799664f0cdc889f673785475393ed5cc0d998f
FluffyKod <potatis@heiman.se>
2019-12-01 21:27:37 +0100

    add example rooms structure

--- p5/code/Rooms.js
+++ p5/code/Rooms.js
@@ -4,7 +4,13 @@ rooms = {
   x: 5,
   y: 8,
   mainText: 'hejhej där',
-  options: []
+  options: [
+    {
+      text: 'option 1 herre',
+      cmd: 'move',
+      values: [5, 8, 'this is some more text']
+    }
+  ]
 }
 
 
