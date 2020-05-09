da741e7f54931956d97452ee47f3f0c44569322a
FluffyKod <potatis@heiman.se>
2020-05-09 10:09:35 +0200

    under construction

--- game.php
+++ game.php
@@ -30,6 +30,20 @@ $completedChapters = 2;
   <body>
 
   <?php
+
+  // Game in progress, show work in progress screen
+  if (is_student_admin() )  { ?>
+
+      <div id="work-in-progress">
+        <div class="box">
+          <h1>Spelet är just nu under konstruktion! Kapitel 2 släpps senare idag.</h1>
+        </div>
+      </div>
+
+  <?php
+    exit();
+  }
+
   // Only show game to logged in users
   if (! is_user_logged_in() ){
     ?>
